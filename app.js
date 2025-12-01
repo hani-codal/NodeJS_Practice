f9d57c80fb80c69886609351a1101439a9d1cc46
hani-codal <hjoshi@codal.com>
2025-12-01 18:53:49 +0530

    Push MVC changes

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const http = require("http");
-const express = require("express");
-const adminRoute = require('./routers/route');
-const shopRoute = require('./routers/shop');
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.use('/admin',adminRoute);
-app.use(shopRoute);
-app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
